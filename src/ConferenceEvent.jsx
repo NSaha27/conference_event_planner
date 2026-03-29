14470807caa405af30a57aedc553cbc0050bafdb
Niladri Saha <sahaniladri27y@gmail.com>
2026-03-29 05:45:29 -0400

    rectify totalCosts variable naming conflict

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -148,7 +148,7 @@ const ConferenceEvent = () => {
           }
         }
       }
-    const totalCost = {
+    const totalCosts = {
         venue: venueTotalCost,
         av: avTotalCost,
         meals: mealsTotalCost
